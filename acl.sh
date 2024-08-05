8af9e8eda6b0edb27f9343e151779c40fb579d19
nikitinya89 <130369330+nikitinya89@users.noreply.github.com>
2024-08-05 14:15:29 +0300

    Create acl.sh

---
+++ acl.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#set -Eeuo pipefail
+
+echo "Enter group name:"
+read group
+echo "Enter Folder Path ( use \\\ for \\ ):"
+read path
+path2=$(echo "$path" | sed 's/\\/\//g')
+
+#desc_folder="\\\nas.semat.ru\\${path} (Read Folder Only)"
+desc_readonly="\\\nas.semat.ru\\${path} (Read Only)"
+desc_modify="\\\nas.semat.ru\\${path} (Modify)"
+desc_fullcontrol="\\\nas.semat.ru\\${path} (Full Control)"
+desc_deny="\\\nas.semat.ru\\${path} (Deny)"
+
+#synogroup --add "$group"_Folder
+#synogroup --descset "$group"_Folder "$desc_folder"
+
+synogroup --add "$group"_ReadOnly
+synogroup --descset "$group"_ReadOnly "$desc_readonly"
+
+synogroup --add "$group"_Modify
+synogroup --descset "$group"_Modify "$desc_modify"
+
+synogroup --add "$group"_FullControl
+synogroup --descset "$group"_FullControl "$desc_fullcontrol"
+
+synogroup --add "$group"_Deny
+synogroup --descset "$group"_Deny "$desc_deny"
+
+#synogroup --memberadd "${group}_Folder" "${group}_ReadOnly"
+#synogroup --memberadd "${group}_Folder" "${group}_Modify"
+#synogroup --memberadd "${group}_Folder" "${group}_FullControl"
+
+
+#synoacltool -add "/volume1/${path2}/" group:"${group}_Folder":allow:r-x---a-R-c--:---n
+synoacltool -add "/volume1/${path2}/" group:"${group}_ReadOnly":allow:r-x---a-R-c--:fd--
+synoacltool -add "/volume1/${path2}/" group:"${group}_Modify":allow:rwxp-DaARWc--:fd--
+synoacltool -add "/volume1/${path2}/" group:"${group}_FullControl":allow:rwxpdDaARWcCo:fd--
+synoacltool -add "/volume1/${path2}/" group:"${group}_Deny":deny:rwxpdDaARWcCo:fd--
+
+cd "/volume1/${path2}/.."
+if [ "$PWD" != "/volume1" ]
+        then
+                synoacltool -add "/volume1/${path2}/.." group:"${group}_ReadOnly":allow:r-x---a-R-c--:---n
+                synoacltool -add "/volume1/${path2}/.." group:"${group}_Modify":allow:r-x---a-R-c--:---n
+                synoacltool -add "/volume1/${path2}/.." group:"${group}_FullControl":allow:r-x---a-R-c--:---n
+fi
+
+
+#upper_folder_group=$(synoacltool -get "/volume1/${path2}/.." | grep -E -o 'group:.*_Folder' | grep -o '[^:]*$')
+#synogroup --memberadd $upper_folder_group "${group}_Folder"
